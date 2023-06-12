cde994e4c2e1916cc3dc7298739f06dfb063d52c
MarcinZbor <marcinzb@gazeta.pl>
2023-06-12 13:06:06 +0200

    ReduxFolder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,7 @@ import ContactsList from './ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import styles from './App.module.css';
 
-const App = () => {
-  const contactsArray = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+
 
   const [contacts, setContacts] = useState(() => {
     return JSON.parse(window.localStorage.getItem('contacts')) || contactsArray;
